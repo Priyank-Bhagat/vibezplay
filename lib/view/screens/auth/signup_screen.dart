64149dea761e0c140bb8b75a755dc3ff5deab689
Priyank-Bhagat <priyankb4dev@proton.me>
2023-10-03 09:59:09 +0530

    removed unused imports

--- lib/view/screens/auth/signup_screen.dart
+++ lib/view/screens/auth/signup_screen.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:vibezplay/constants.dart';
 import 'package:vibezplay/controller/auth_controller.dart';
 import 'package:vibezplay/view/screens/auth/login_screen.dart';
